3bdbc3472b40226d1944d0509eb600983cefd286
GenkiOsawa <taiiku39@gmail.com>
2021-05-23 16:34:27 +0900

    テストの準備

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "roots": [
+    "./src"
+  ],
+  "testMatch": [
+    "**/__tests__/**/*.+(ts|js)",
+    "**/?(*.)+(spec|test).+(ts|js)"
+  ],
+  "transform": {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  "preset": "ts-jest",
+  // "testEnvironment": "./prisma/prisma-test-environment.js"
+}
---
+++ src/tests/index.test.ts
@@ -0,0 +1,5 @@
+describe("テストのテスト", () => {
+  it("テスト", () => {
+    expect(true).toBe(true)
+  })
+})
